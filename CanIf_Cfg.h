497dbbe0cf54691d80d8c74a22aa01c8aa71366f
Eman Megahed <50622434+eman1233@users.noreply.github.com>
2020-08-25 18:52:32 +0200

    Add files via upload

---
+++ CanIf_Cfg.h
@@ -0,0 +1,11 @@
+#ifndef __CanIf_Cfg_h__
+#define __CanIf_Cfg_h__
+#define STD_ON 1
+#define STD_OFF 0
+#define CanIfDevErrorDetect                   STD_OFF
+#define CanIfPublicSetDynamicTxIdApi          STD_OFF
+#define CanIfPublicReadTxPduNotifyStatusApi   STD_OFF
+#define CanIfTxPduReadNotifyStatus            STD_OFF
+#define CanIfPublicReadRxPduNotifyStatusApi   STD_OFF
+#define CanIfRxPduReadNotifyStatus            STD_OFF
+#endif
